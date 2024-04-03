a497ea981f5350cdcf2387af2860963989768e09
Murtaza Tankiwala <115138620+murtazatankiwala456@users.noreply.github.com>
2024-04-03 21:18:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+$("#box-1").click(function () {
+  $("#text-1").slideToggle();
+  $("#img-1").toggleClass("img-rotate");
+});
+
+$("#box-2").click(function () {
+  $("#text-2").slideToggle();
+  $("#img-2").toggleClass("img-rotate");
+});
